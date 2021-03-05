89922b9b8d09b0c3ed1ae7c3d8e77e42440eb388
kittipong199 <68522978+kittipong199@users.noreply.github.com>
2021-03-05 16:14:20 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { Lad4Component } from './lad4/lad4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lad4Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/lad4/lad4.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lad4',
+  templateUrl: './lad4.component.html',
+  styleUrls: ['./lad4.component.css']
+})
+export class Lad4Component {
+  arrNumber: number[] = [1,2,3,4,5,6,7,8,9,10];
+}
